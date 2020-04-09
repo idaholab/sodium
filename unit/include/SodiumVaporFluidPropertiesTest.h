b4743e3aa185504fb16b719be7aa68eac0d60c90
Alex Lindsay <alexlindsay239@gmail.com>
2020-04-09 14:50:41 -0700

    Update to AD 2.0
    Refs idaholab/moose#14547

--- unit/include/SodiumVaporFluidPropertiesTest.h
+++ unit/include/SodiumVaporFluidPropertiesTest.h
@@ -13,7 +13,7 @@ protected:
   {
     InputParameters uo_pars = _factory.getValidParams("SodiumVaporFluidProperties");
     _fe_problem->addUserObject("SodiumVaporFluidProperties", "fp", uo_pars);
-    _fp = &_fe_problem->getUserObjectTempl<SodiumVaporFluidProperties>("fp");
+    _fp = &_fe_problem->getUserObject<SodiumVaporFluidProperties>("fp");
   }
 
   const SodiumVaporFluidProperties * _fp;
