dc842efb90ee592c3461eaf41c4be7733b10f602
hansje <joshua.hansel@inl.gov>
2020-06-08 11:02:49 -0600

    Added triple-point temperature
    Closes #20

--- unit/src/SodiumTwoPhaseFluidPropertiesTest.C
+++ unit/src/SodiumTwoPhaseFluidPropertiesTest.C
@@ -6,14 +6,17 @@ TEST_F(SodiumTwoPhaseFluidPropertiesTest, test)
   Real T = 1200.;  // K
   Real p = 101325; // Pa
 
-  // Tsat + derivatives
+  // T_triple
+  REL_TEST(_fp->T_triple(), 370.98, REL_TOL_SAVED_VALUE);
+
+  // Tsat
   REL_TEST(_fp->T_sat(p), 1154.6031110050355, REL_TOL_SAVED_VALUE);
   DERIV_TEST_1D(_fp->T_sat, _fp->dT_sat_dp, p, REL_TOL_DERIVATIVE);
 
   // Psat
   REL_TEST(_fp->p_sat(T), 150357.12693001426, REL_TOL_SAVED_VALUE);
 
-  // Psat
+  // sigma
   REL_TEST(_fp->sigma_from_T(T), 0.11534571734609324, REL_TOL_SAVED_VALUE);
   DERIV_TEST_1D(_fp->sigma_from_T, _fp->dsigma_dT_from_T, T, REL_TOL_DERIVATIVE);
 }
